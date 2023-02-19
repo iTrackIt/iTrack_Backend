3beaec3bb52d39b330ee176ff0812b383fcf94c0
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2023-02-19 21:18:01 +0200

    Added the password hashing and matching methods , finished the educator model

--- src/main/java/rw/iTrack/Application/v1/models/Educator.java
+++ src/main/java/rw/iTrack/Application/v1/models/Educator.java
@@ -1,32 +1,60 @@
 package rw.iTrack.Application.v1.models;
 
+import com.sun.istack.NotNull;
+import io.swagger.annotations.ApiModelProperty;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import rw.iTrack.Application.v1.enums.Gender;
 
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "educators" , uniqueConstraints = {@UniqueConstraint(columnNames = {"email"}) , @UniqueConstraint(columnNames = {"nat_id"})})
 public class Educator {
-    private Long ed_id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID ed_id;
+    @NotNull
+    @ApiModelProperty("The full names of the Educator")
     private String fullNames;
+    @NotNull
+    @ApiModelProperty("The username of the Educator")
     private String username;
+
+    @NotNull
+    @ApiModelProperty("The email of the educator")
+    @Column(name = "email")
+    private String email;
+    @NotNull
+    @ApiModelProperty("The national id of the educator")
+    @Column(name = "nat_id")
     private String national_id;
+    @NotNull
+    @ApiModelProperty("The gender of the educator male or female?")
     private Gender gender;
+    @NotNull
+    @ApiModelProperty("The password of the educator")
     private String password;
 
     public Educator(){
 
     }
 
-    public Educator(String fullNames, String username, String national_id, Gender gender, String password) {
+    public Educator(String fullNames, String username, String email ,  String national_id, Gender gender, String password) {
         this.fullNames = fullNames;
         this.username = username;
         this.national_id = national_id;
         this.gender = gender;
         this.password = password;
+        this.email = email;
     }
 
-    public Long getEd_id() {
+    public UUID getEd_id() {
         return ed_id;
     }
 
-    public void setEd_id(Long ed_id) {
+    public void setEd_id(UUID ed_id) {
         this.ed_id = ed_id;
     }
 
@@ -44,6 +72,14 @@ public class Educator {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getNational_id() {
